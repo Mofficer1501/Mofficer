aafd34d889421dc276ca6dcbcd07105645e31aa0
Mofficer1501 <149263720+Mofficer1501@users.noreply.github.com>
2023-10-28 23:32:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String stroka = scan.nextLine();
+        String[] str = stroka.split(" ");
+        boolean flag=false;
+
+        for (int i = 0; i <= str.length; i++) {
+            if (str[i].length() > 3 && str[i].charAt(0) == 'а'){
+                System.out.println(str[i]);
+                flag=true;
+            }
+        }
+        if (!flag) System.out.println("Слов на 'а' больше 3 символов не найдено");
+        scan.close();
+    }
+}
